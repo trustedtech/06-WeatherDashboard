2c38b3ca19ba6f140ad3351a8b9ef199928a05cd
trusted.tech <brian@trusted.tech>
2020-01-28 21:06:44 -0500

    Modified project scope and details to suit available APIs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,24 @@
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+var apiKey = "&APPID=2b853bacb586a6ba502bc5abb5c5ba1b";
+var unitsParam = "&units=imperial";
+
+queryURL = weatherURL + "Orlando" + apiKey + unitsParam;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+console.log(response);
+});
+
+queryURL = forecastURL + "Orlando" + apiKey + unitsParam;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+console.log(response);
+});
 
 // Open Weather API-Key:  2b853bacb586a6ba502bc5abb5c5ba1b
