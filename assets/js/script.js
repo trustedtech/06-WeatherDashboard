0a766bfd3bd6e08c73c2879e2509c2ef75aaa4f0
trusted.tech <brian@trusted.tech>
2020-01-29 19:22:40 -0500

    Restore missing JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
-// API keys that may be useful
-var APIKeyNutritionix = "543062e2f89a916d71461f4c8c3befda"
-var APIKeyUSDA = "L3tHPcFfPTSKSiXbCfKg2KltRijm4Dlj6PL2hK7I"
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+var apiKey = "&APPID=2b853bacb586a6ba502bc5abb5c5ba1b";
+var unitsParam = "&units=imperial";
 
-
-// backend test code
-var input = "butter"
+queryURL = weatherURL + "Orlando" + apiKey + unitsParam;
 
 $.ajax({
-    url: "https://api.nal.usda.gov/fdc/v1/search?api_key="+APIKeyUSDA+"\&generalSearchInput="+input,
-
+    url: queryURL,
     method: "GET"
-}).then(function (response) {
-	console.log(response);
+  }).then(function(response) {
+console.log(response);
 });
+
+queryURL = forecastURL + "Orlando" + apiKey + unitsParam;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+console.log(response);
+});
+
+// Open Weather API-Key:  2b853bacb586a6ba502bc5abb5c5ba1b
